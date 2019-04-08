adabad6a9f24b4fbccab6b8cb8fd170234153eac
Tomorenge <tomorenge3@gmail.com>
2019-04-08 18:27:19 +0300

    wrote logic on App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,22 @@
-import java.util.Scanner*;
+import java.io.Console;
+import java.util.Scanner;
 
 
 public class App {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 to encrypt and 2 to encrypt");
+        int choice=scanner.nextInt();
+        if(choice==1) {
+
+        System.out.println("Enter the message to be encrypted in ceasar cipher");
+        String message = scanner.nextLine();
+
+        System.out.println("Enter the key ");
+        int key = scanner.nextInt();
+
+        System.out.println(CeaserCipher.encrypt(message,key));
+
     }
 }
 
