030090c037e7231147f8473fc0f25fe3514abd63
Tomorenge <tomorenge3@gmail.com>
2019-04-09 11:33:37 +0300

    added decryption logic to App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,4 @@
-import java.io.Console;
+
 import java.util.Scanner;
 
 
@@ -7,16 +7,34 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 1 to encrypt and 2 to encrypt");
         int choice=scanner.nextInt();
-        if(choice==1) {
+        scanner.nextLine();
 
-        System.out.println("Enter the message to be encrypted in ceasar cipher");
-        String message = scanner.nextLine();
+        if(choice == 1)
+        {
+            System.out.println("Enter the message to be encrypted in ceasar cipher");
+            String message = scanner.nextLine();
 
-        System.out.println("Enter the key ");
-        int key = scanner.nextInt();
+            System.out.println("Enter the key ");
+            int key = scanner.nextInt();
 
-        System.out.println(CeaserCipher.encrypt(message,key));
+            System.out.println(CeaserCipher.encrypt(message,key));
+        }
+        else if(choice == 2)
+        {
+            System.out.println("Enter the message to be decrypted in ceasar cipher");
+            String message = scanner.nextLine();
+
+            System.out.println("Enter the key ");
+            int key = scanner.nextInt();
+//
+
+            System.out.println(CeaserCipher.encrypt(message,key));
+        }
+            System.out.println(" decrypt");
+        }
+
+
 
     }
-}
+
 
